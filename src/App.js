01c02deeb705a267825e899ff20d27ed67dadfc5
LoneWolf36 <runefaizan36@gmail.com>
2018-06-25 01:00:52 +0530

    Refactored

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import './App.css'
 import {  BrowserRouter as Router, Route, Link, Switch, Redirect  } from 'react-router-dom'
-import Home from './components/Home/Home'
-import Projects from './components/Projects/Projects'
+import Home from './containers/Home/Home'
+import Projects from './containers/Projects/Projects'
 
 class App extends Component {
 	render() {
