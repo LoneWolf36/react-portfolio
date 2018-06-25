95d6009eb3aada3c2fb22d5f0846c1a878db4c90
LoneWolf36 <runefaizan36@gmail.com>
2018-06-25 16:13:43 +0530

    Created build

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './AboutMe.css'
 import Zoom from 'react-reveal/Zoom'
+import Fade from 'react-reveal/Fade'
+import Bounce from 'react-reveal/Bounce'
 
 const Home = () => {
 	return (
@@ -11,7 +13,42 @@ const Home = () => {
 				<img src={require(('../../assets/faiz.jpg'))} alt='Faizan Khan' className='img-fluid rounded' />
 			</div>
 			<div className='mt-5'>
-				<Zoom><p>I am an enthusiastic fresher who is passionate about learning new and upcoming technologies and work hard for the growth of the organization as well as personal growth.</p></Zoom>
+				<Fade>
+					<h1 className='text-center m-5'>About Me</h1>
+				</Fade>
+				<Zoom>
+					<section>
+						<div className="card rounded mb-5">
+							<div className="card-body text-center">
+								I am an enthusiastic fresher who is passionate about learning new and upcoming technologies and work hard for the growth of the organization as well as personal growth.
+							</div>
+						</div>
+					</section>
+				</Zoom>
+				<Zoom>
+					<section>
+						<div className="card rounded mb-5">
+							<div className="card-body text-center">
+								My curiosity to learn new things keeps me driven to learn more in this ever growing world, filled with great technologies.
+							</div>
+						</div>
+					</section>
+				</Zoom>
+				<Zoom>
+					<section>
+						<div className="card rounded mb-5">
+							<div className="card-body text-center">
+								<h1 className=''>The qualities that make me stand out are: </h1>
+								<Bounce><p>Curiosity</p></Bounce>
+								<Bounce><p>Adaptability</p></Bounce>
+								<Bounce><p>Communication skills</p></Bounce>
+								<Bounce><p>Hard working</p></Bounce>
+								<Bounce><p>Friendly and supportive</p></Bounce>
+								<Bounce><p>My smile : )</p></Bounce>
+							</div>
+						</div>
+					</section>
+				</Zoom>
 			</div>
 		</div>
 	)
